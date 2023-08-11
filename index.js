056926005e6175d1620f8ed360ec1efa6d290c0b
MDTufan <129486543+MDTufan@users.noreply.github.com>
2023-08-11 11:33:57 +0600

    Add files via upload
    fs,os.path

---
+++ index.js
@@ -0,0 +1,20 @@
+// const fs =require('fs');
+// fs.writeFile( 'demo.txt','i am tufan ali', (err)=>{
+//     if(err){
+//       console.log('not found')
+//     }else{
+//         console.log('successfull')
+//     }
+// })
+// const fs =require('fs');
+// fs.appendFile( 'demo.txt','i am toli', (err)=>{
+//     if(err){
+//       console.log('not found')
+//     }else{
+//         console.log('successfull')
+//     }
+// })
+
+
+const os =require('os');
+console.log(os.userinfo())
